42b0dd627d59b3dd68d42ae22a2fb6d10c781712
jandro15951 <79611125+jandro15951@users.noreply.github.com>
2021-08-17 22:27:01 -0600

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,91 @@
+function responderChat(id, pregunta){
+
+    
+//document.getElementById(id.toString()).style.display="none";
+
+var p="";
+
+
+
+if(id=='001'){
+    var pregunta=document.getElementById("seccionRespuestas")
+    .innerHTML=p+"<div class='flexPreguntaChat'><div class='preguntaChatD'><label class='textoPregunta'>"+pregunta+"</label></div></div>"+
+    "<div class='flexRespuestaChat'><div class='RespuestaChatD'><label class='textoPregunta'>"+'Me inicie en el mundo de la programacion en '+
+    'el 2018, tengo 3 años siendo autodidacta y 2 años estudiando en la Universidad'+"</label></div></div>";
+}
+if(id=='002'){
+    var pregunta=document.getElementById("seccionRespuestas")
+    .innerHTML=p+"<div class='flexPreguntaChat'><div class='preguntaChatD'><label class='textoPregunta'>"+pregunta+"</label></div></div>"+
+    "<div class='flexRespuestaChat'><div class='RespuestaChatD'><label class='textoPregunta'>"+'He laborado estilo freelancer para la empresa de  '+
+    'un profesor en mi Universidad, cuyo nombre de empresa es Cyfcon'+"</label></div></div>";
+
+}
+if(id=='003'){
+    var pregunta=document.getElementById("seccionRespuestas")
+    .innerHTML=p+"<div class='flexPreguntaChat'><div class='preguntaChatD'><label class='textoPregunta'>"+pregunta+"</label></div></div>"+
+    "<div class='flexRespuestaChat'><div class='RespuestaChatD'><label class='textoPregunta'>"+'Debo admitir que amo la coca-cola jejeje  '+
+    "</label></div></div>";
+
+}
+if(id=='004'){
+    var pregunta=document.getElementById("seccionRespuestas")
+    .innerHTML=p+"<div class='flexPreguntaChat'><div class='preguntaChatD'><label class='textoPregunta'>"+pregunta+"</label></div></div>"+
+    "<div class='flexRespuestaChat'><div class='RespuestaChatD'><label class='textoPregunta'>"+'El lenguaje de programacion donde tengo mejores bases es C# '+
+    'debido a que fue mi lenguaje inicial, posteriormente aprendi ASP .Net Core donde me desenvuelvo de una buena manera'+"</label></div></div>";
+
+}
+if(id=='005'){
+    var pregunta=document.getElementById("seccionRespuestas")
+    .innerHTML=p+"<div class='flexPreguntaChat'><div class='preguntaChatD'><label class='textoPregunta'>"+pregunta+"</label></div></div>"+
+    "<div class='flexRespuestaChat'><div class='RespuestaChatD'><label class='textoPregunta'>"+'Actualmente tengo residencia en Atenas, Alajuela sin embargo '+
+    'no tengo mayor problema en desmplazarme a cualquier zona del GAM'+"</label></div></div>";
+
+}
+if(id=='006'){
+    var pregunta=document.getElementById("seccionRespuestas")
+    .innerHTML=p+"<div class='flexPreguntaChat'><div class='preguntaChatD'><label class='textoPregunta'>"+pregunta+"</label></div></div>"+
+    "<div class='flexRespuestaChat'><div class='RespuestaChatD'><label class='textoPregunta'>"+'Mi mayor fortaleza radica en mi rapido acoplamiento a nuevas tecnologias '+
+    ' y resalto mi trabajo en equipo, soy una persona amistosa :) '+"</label></div></div>";
+
+}
+
+
+if(id=='007'){
+    var pregunta=document.getElementById("seccionRespuestas")
+    .innerHTML=p+"<div class='flexPreguntaChat'><div class='preguntaChatD'><label class='textoPregunta'>"+pregunta+"</label></div></div>"+
+    "<div class='flexRespuestaChat'><div class='RespuestaChatD'><label class='textoPregunta'>"+'Como todo buen programador, en mi tiempo libre me gusta jugar videojuegos y realizar recorridos largos en bicicleta '+
+    ''+"</label></div></div>";
+
+}
+
+if(id=='008'){
+    var pregunta=document.getElementById("seccionRespuestas")
+    .innerHTML=p+"<div class='flexPreguntaChat'><div class='preguntaChatD'><label class='textoPregunta'>"+pregunta+"</label></div></div>"+
+    "<div class='flexRespuestaChat'><div class='RespuestaChatD'><label class='textoPregunta'>"+'En 5 o 10 años me veo trabajando en una gran empresa, rodeado de compañeros programadores '+
+    ', asi mismo en mis metas a futuro esta entrar de lleno a Inteligencia Artificial. '+"</label></div></div>";
+
+}
+if(id=='009'){
+    var pregunta=document.getElementById("seccionRespuestas")
+    .innerHTML=p+"<div class='flexPreguntaChat'><div class='preguntaChatD'><label class='textoPregunta'>"+pregunta+"</label></div></div>"+
+    "<div class='flexRespuestaChat'><div class='RespuestaChatD'><label class='textoPregunta'>"+'Considero que la mejor forma de afrontar el fracaso, es no darse por vencido, en un mundo como lo es la programacion, se encontraran problemas dia tras dia, pero considero que es esto, lo que nos hace aprender mas y mas, asi que no se deben como fracasos, si no, como una forma mas de aprender. '+
+    ''+"</label></div></div>";
+
+}
+if(id=='010'){
+    var pregunta=document.getElementById("seccionRespuestas")
+    .innerHTML=p+"<div class='flexPreguntaChat'><div class='preguntaChatD'><label class='textoPregunta'>"+pregunta+"</label></div></div>"+
+    "<div class='flexRespuestaChat'><div class='RespuestaChatD'><label class='textoPregunta'>"+'Considero que si, hasta el momento he visto reflejado el esfuerzo que he puestos en mis ultimos años, cosa que me motiva mucho! Estoy seguro que no hay mas que cosas buenas por delante.'+
+    ''+"</label></div></div>";
+
+}
+
+
+
+
+
+
+
+
+
+}
